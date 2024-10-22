8702cde36d3121f5fc03b9598643fcc542af2456
cyq <cyq@qq.com>
2024-10-22 17:29:18 +0800

    message dialog content editable

--- rim-web/src/components/Main/BottomChat.jsx
+++ rim-web/src/components/Main/BottomChat.jsx
@@ -4,36 +4,29 @@ import ChatDialog from "./ChatDialog";
 
 const BottomChat = () => {
   return (
-    <div id="bottom-chat" className="flex flex-col h-full ">
-      <div id="chat-message-container" className="block overflow-y-scroll p-2">
+    <div id="bottom-chat" className="flex flex-col h-full">
+      <div
+        id="chat-message-container"
+        className="overflow-y-scroll px-4 py-2 flex flex-col h-full"
+      >
+        dsfsdf message-push-688c4db4b-4xn5d 209m message-push-688c4db4b-7pj2b a
+        message-push-688c4db4b-bm8m5 401m message-push-688c4db4b-f5hv9 285m
+        message-push-688c4db4b-fhgjl 85m 88m message-push-688c4db4b-bm8m5 401m
+        message-push-688c4db4b-f5hv9 285m message-push-688c4db4b-fhgjl 85m 88m
+        message-push-688c4db4b-bm8m5 401m message-push-688c4db4b-f5hv9 285m
+        <br />
+        message-push-688c4db4b-f5hv9 285m <br />
+        message-push-688c4db4b-f5hv9 285m <br />
+        message-push-688c4db4b-f5hv9 285m <br />
+        message-push-688c4db4b-f5hv9 285m <br />
+        message-push-688c4db4b-f5hv9 285m <br />
+        message-push-688c4db4b-f5hv9 285m <br />
         <ChatMessage />
-        <p>0o</p>
-        <p>0o</p>
-        <p>0o</p>
-        <p>0o</p>
-        <p>0o</p>
-        <p>0o</p>
-        <p>0o</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>d</p>
-        <p>c</p>
+        <div className="flex-grow"></div>
       </div>
       <hr />
-      <div id="chat-dialog-container" className="flex flex-col mt-1  h-32 p-2">
+      <div id="chat-dialog-container" className="mt-1 m-h-32 px-4 py-2">
         <ChatDialog />
-        <div className="flex-grow"></div>
       </div>
     </div>
   );
--- rim-web/src/components/Main/ChatDialog.jsx
+++ rim-web/src/components/Main/ChatDialog.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
+import {
+  FaceSmileIcon,
+  AtSymbolIcon,
+  PlusCircleIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
+
 const ChatDialog = () => {
   return (
-    <div>
-      chat dialog<div>chat dialog</div>
-      <div>chat dialog</div>
+    <div
+      id="chat-dialog"
+      className="rounded border border-slate-500 p-4 min-h-16 max-h-36"
+      contentEditable={true}
+    >
+      <div className="relative ml-4">
+        &nbsp;
+        <div className="flex w-20 bg-slate-400 absolute bottom-0 right-0">
+          <FaceSmileIcon className="w-5 h-5" />
+          <AtSymbolIcon className="w-5 h-5" />
+          <PlusCircleIcon className="w-5 h-5" />
+          <PaperAirplaneIcon className="w-5 h-5" />
+        </div>
+      </div>
     </div>
   );
 };
